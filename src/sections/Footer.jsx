34af544ca3c661e8b9c8d45e208331ef69780aca
erbuzzel <erbuzzel@mtu.edu>
2025-05-10 10:43:13 -0500

    Deploy mechanisms

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Footer() {
   return <footer>
@@ -7,7 +8,10 @@ export default function Footer() {
         <div className='flex gap-10 p-4 justify-center text-white'>
             <a href="https://www.linkedin.com/in/elisebuzzell/" className=' p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-blue-700 text-white' target='_blank'>
             <FaLinkedin /></a>
+            <a href="https://github.com/ebuzz00" className=' p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-black text-white' target='_blank'>
+            <FaGithubSquare /></a>
         </div>
+      
         <p>@ 2025 Buzzell. All rights reserved. </p>
     </div>
   </footer> ;
